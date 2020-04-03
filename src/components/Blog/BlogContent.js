fde7ccd7aea90c902ae4e220ec07ba01f80dd2ab
Batchimeg.a <batchimeg01@gmail.com>
2020-04-03 17:49:55 +0800

    fix blog

--- src/components/Blog/BlogContent.js
+++ src/components/Blog/BlogContent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import moment from "moment";
-import PageWrapper from "../PageWrapper";
-import SidebarList from './SidebarList';
 import Link from "next/link";
 import { prefixer, getData } from '../../utils';
 import Config from "../../config";
@@ -29,62 +27,55 @@ class Content extends React.Component {
 
     return (
       <>
-        <PageWrapper classes={'blog-page-content-area sp-y'}>
-          <div className='col-lg-9 null'>
-            <div className='blog-content-wrapper false'>
-              <div className="row mtn-30">
-                {posts.map(post => {
-                  return (
-                    <div key={post.id} className='col-md-6'>
-                      <div className="blog-item">
-                        <figure className="blog-thumb">
-                          <Link href={prefixer(`/blog-more/${post.slug}`)}>
-                            <a>
-                              <img src={getData(post._embedded, 'image')} alt={post.title} />
-                            </a>
-                          </Link>
-                        </figure>
-                        <div className="blog-content">
-                          <h2 className="h6">
-                            <Link href={prefixer(`/blog-more/${post.slug}`)}>
-                              <a>
-                                <div
-                                  className={"post-content"}
-                                  dangerouslySetInnerHTML={{
-                                    __html: post.title.rendered
-                                  }}
-                                />
-                              </a>
-                            </Link>
-                          </h2>
-                          <div
-                            className={"post-content"}
-                            dangerouslySetInnerHTML={{
-                              __html: post.excerpt.rendered
-                            }}
-                          />
-                          <div className="blog-meta">
-                            <Link href={prefixer(`/blog-more/${post.slug}`)}>
-                              <a>
-                                By: {getData(post._embedded, 'author').name}
-                              </a>
-                            </Link>
-                            <Link href={prefixer(`/blog-more/${post.slug}`)}>
-                              <a>
-                                {moment(post.date).format('ll')}
-                              </a>
-                            </Link>
-                          </div>
-                        </div>
+        <div className='col-lg-9 null'>
+          <div className='blog-content-wrapper false'>
+            <div className="row mtn-30">
+              {posts.map(post => (
+                <div key={post.slug} className='col-md-6'>
+                  <div className="blog-item">
+                    <figure className="blog-thumb">
+                      <Link href={prefixer(`/blog-more/${post.slug}`)}>
+                        <a>
+                          <img src={getData(post._embedded, 'image')} alt={post.title} />
+                        </a>
+                      </Link>
+                    </figure>
+                    <div className="blog-content">
+                      <h2 className="h6">
+                        <Link href={prefixer(`/blog-more/${post.slug}`)}>
+                          <a>
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: post.title.rendered
+                              }}
+                            />
+                          </a>
+                        </Link>
+                      </h2>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: post.excerpt.rendered
+                        }}
+                      />
+                      <div className="blog-meta">
+                        <Link href={prefixer(`/blog-more/${post.slug}`)}>
+                          <a>
+                            By: {getData(post._embedded, 'author').name}
+                          </a>
+                        </Link>
+                        <Link href={prefixer(`/blog-more/${post.slug}`)}>
+                          <a>
+                            {moment(post.date).format('ll')}
+                          </a>
+                        </Link>
                       </div>
                     </div>
-                  );
-                })}
-              </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-          <SidebarList />
-        </PageWrapper>
+        </div>
       </>
     );
   }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,8 @@ import BlogContent from "../components/Blog/BlogContent";
 import CallToAction from "../components/CallToAction";
 import MobileMenu from "../components/MobileMenu";
 import PageHeader from "../components/PageHeader";
+import SidebarList from '../components/Blog/SidebarList';
+import PageWrapper from "../components/PageWrapper";
 import { prefixer } from '../utils';
 
 export default function BlogPage() {
@@ -15,7 +17,10 @@ export default function BlogPage() {
           title={'Blog'}
           content={'Businex always try to provide the best Business Solutions for Clients to grow up their Business very sharply and smoothly.'}
         />
-        <BlogContent />
+        <PageWrapper classes={'blog-page-content-area sp-y'}>
+          <BlogContent />
+          <SidebarList />
+        </PageWrapper>
         <CallToAction />
       </Layout>
       <MobileMenu />
