6596f4d29bda41fe2eb88e91c7a0b2b0c10d1274
Batchimeg.a <batchimeg01@gmail.com>
2020-04-04 17:50:39 +0800

    commit book-now

--- src/pages/book-now.js
+++ src/pages/book-now.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Head from 'next/head';
 
-import TagManager from 'react-gtm-module';
+// import TagManager from 'react-gtm-module';
 
-// const wp = new WPAPI({ endpoint: Config().apiUrl });
-const tagManagerArgs = {
-  gtmId: 'GTM-NN7QKCX'
-}
+// // const wp = new WPAPI({ endpoint: Config().apiUrl });
+// const tagManagerArgs = {
+//   gtmId: 'GTM-NN7QKCX'
+// }
 
 export default function BookNow() {
-  TagManager.initialize(tagManagerArgs)
+  // TagManager.initialize(tagManagerArgs)
 
   return (
     <>
-      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden;"></iframe>` }} />
+      <p>Book now</p>
+      {/* <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden;"></iframe>` }} />
 
-      <div data-erxes-embed="qXsQ2D" style={{ width: "900px", height: "800px" }}></div>
+      <div data-erxes-embed="qXsQ2D" style={{ width: "900px", height: "800px" }}></div> */}
     </>
   );
 }
