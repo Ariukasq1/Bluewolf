53402cc78f5ec69588c27ca98b2519e97a2105f2
Batchimeg.a <batchimeg01@gmail.com>
2020-04-28 18:26:32 +0800

    fix bug

--- src/components/Festival.js
+++ src/components/Festival.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import Config from "../config";
 import { LanguageConsumer } from './LanguageContext';
 
-export default class Festival extends React.Component {
+class Festival extends React.Component {
   constructor(props) {
     super(props);
 
