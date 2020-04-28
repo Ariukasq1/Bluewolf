9891610cb5a9723ed91355f312263728b3827884
Batchimeg.a <batchimeg01@gmail.com>
2020-04-28 21:32:53 +0800

    update lang url

--- src/components/layouts/HeaderConfig.js
+++ src/components/layouts/HeaderConfig.js
@@ -11,10 +11,10 @@ function HeaderConfig({ onChangeLang }) {
         <div className="header-action mt-lg-3 text-right">
             <a href="99110303" className="phone"> (+976) 99110303 </a>
             <div className='lang-action'>
-                <a onClick={onChangeLang.bind(this, '')} ><img src={prefixer('/images/lang/eng.png')} alt='english' /></a>
-                <a onClick={onChangeLang.bind(this, 'ja')} ><img src={prefixer('/images/lang/japan.png')} alt='english' /></a>
-                <a onClick={onChangeLang.bind(this, 'ru')} ><img src={prefixer('/images/lang/rus.png')} alt='english' /></a>
-                <a onClick={onChangeLang.bind(this, 'zh')} ><img src={prefixer('/images/lang/china.png')} alt='english' /></a>
+                <a onClick={onChangeLang.bind(this, 'en')} ><img src={prefixer('/images/lang/eng.png')} alt='english' /></a>
+                <a onClick={onChangeLang.bind(this, 'ja')} ><img src={prefixer('/images/lang/japan.png')} alt='japan' /></a>
+                <a onClick={onChangeLang.bind(this, 'ru')} ><img src={prefixer('/images/lang/rus.png')} alt='russian' /></a>
+                <a onClick={onChangeLang.bind(this, 'zh')} ><img src={prefixer('/images/lang/china.png')} alt='china' /></a>
                 <button onClick={MobileMenuHandler} className="btn-menu d-lg-none"><i className="fa fa-bars" /></button>
             </div>
         </div>
